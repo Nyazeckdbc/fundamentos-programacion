ab4e745ed956458f3bb9d6e19cc7972ee19628c9
Juan <nyazeckdbc@gmail.com>
2020-07-02 16:39:41 -0500

    trece-g completado

---
+++ trece-g.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int indice, i=1, serie;
+
+    cout<<"Ingrese un indice para la serie: ";
+    cin>>indice;
+    while(i<=indice){
+
+        if(i%2==0){
+            serie = (i-1)/-1;
+            cout<<serie<<" ";
+        }
+        else
+        {
+            serie = i-1;
+            cout<<serie<<" ";
+        }
+
+        i++;
+    }
+
+    return 0;
+}
