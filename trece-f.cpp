7b70e19c7fbd355e46f3b7071ae7b654d8f14f87
Juan <nyazeckdbc@gmail.com>
2020-07-03 01:27:19 -0500

    correcion trece

--- trece-f.cpp
+++ trece-f.cpp
@@ -5,65 +5,35 @@ using namespace std;
 
 int main(){
 
-    int indice, i=5, serie, posicionUno=2, posicionDos=3, posicionTres=5, posicionCuatro=7;
-    int j=5, k=6,l=7, m=8;     
+    int indice, i=1, posicionUno=0, posicionDos=0, posicionTres=0, posicionCuatro=0;
+    int j=1, k=2,l=3, m=4;     
 
     cout<<"Ingrese un indice para la serie: ";
     cin>>indice;
-    if(indice==1){
-        cout<<posicionUno<<" ";
-    }
-    else if(indice==2){
-        cout<<posicionUno<<" ";
-        cout<<posicionDos<<" ";
-    }
-    else if(indice==3){
-        cout<<posicionUno<<" ";
-        cout<<posicionDos<<" ";
-        cout<<posicionTres<<" ";
-    }
-    else if(indice==4){
-        cout<<posicionUno<<" ";
-        cout<<posicionDos<<" ";
-        cout<<posicionTres<<" ";
-        cout<<posicionCuatro<<" ";
-    }
-    else{
-        cout<<posicionUno<<" ";
-        cout<<posicionDos<<" ";
-        cout<<posicionTres<<" ";
-        cout<<posicionCuatro<<" ";
-        while(i<=indice){
+    
+    while(i<=indice){
             if(i==j){
-                serie = posicionUno +2;
-                cout<<serie<<" ";
-                j+=4;
                 posicionUno+=2;
+                cout<<posicionUno<<" ";
+                j+=4;
             }
             else if(i==k){
-                serie = posicionDos +3;
-                cout<<serie<<" ";
-                k+=4;
                 posicionDos+=3;
+                cout<<posicionDos<<" ";
+                k+=4;
             }
             else if(i==l){
-                serie = posicionTres +5;
-                cout<<serie<<" ";
-                l+=4;
                 posicionTres+=5;
-
+                cout<<posicionTres<<" ";
+                l+=4;
             }
             else if(i==m){
-                serie = posicionCuatro +7;
-                cout<<serie<<" ";
-                m+=4;
                 posicionCuatro+=7;
+                cout<<posicionCuatro<<" ";
+                m+=4;
             }
-
-
             i++;
         }
-    }
 
     return 0;
 }
