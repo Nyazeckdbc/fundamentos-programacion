a733221aefbf5c7580b97becb1f355a12b4892d5
Juan <nyazeckdbc@gmail.com>
2020-07-03 01:04:10 -0500

    punt 13 terminado

--- trece-f.cpp
+++ trece-f.cpp
@@ -1,30 +1,68 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main(){
 
-    int indice, i=1, serie;
+    int indice, i=5, serie, posicionUno=2, posicionDos=3, posicionTres=5, posicionCuatro=7;
+    int j=5, k=6,l=7, m=8;     
 
     cout<<"Ingrese un indice para la serie: ";
     cin>>indice;
-    while(i<=indice){
+    if(indice==1){
+        cout<<posicionUno<<" ";
+    }
+    else if(indice==2){
+        cout<<posicionUno<<" ";
+        cout<<posicionDos<<" ";
+    }
+    else if(indice==3){
+        cout<<posicionUno<<" ";
+        cout<<posicionDos<<" ";
+        cout<<posicionTres<<" ";
+    }
+    else if(indice==4){
+        cout<<posicionUno<<" ";
+        cout<<posicionDos<<" ";
+        cout<<posicionTres<<" ";
+        cout<<posicionCuatro<<" ";
+    }
+    else{
+        cout<<posicionUno<<" ";
+        cout<<posicionDos<<" ";
+        cout<<posicionTres<<" ";
+        cout<<posicionCuatro<<" ";
+        while(i<=indice){
+            if(i==j){
+                serie = posicionUno +2;
+                cout<<serie<<" ";
+                j+=4;
+                posicionUno+=2;
+            }
+            else if(i==k){
+                serie = posicionDos +3;
+                cout<<serie<<" ";
+                k+=4;
+                posicionDos+=3;
+            }
+            else if(i==l){
+                serie = posicionTres +5;
+                cout<<serie<<" ";
+                l+=4;
+                posicionTres+=5;
 
-        if((i+1)%2==0){
-            serie= (i+3)/2;
-            cout<<serie<<" ";
-        }
-        else if((i+1)%3==0){
-            cout<<"falta-3 ";
-        }
-        else if((i+2)%5==0){
-            cout<<"falta-5 ";
-        }
-        else{
-            cout<<"falta-7 ";
-        }
+            }
+            else if(i==m){
+                serie = posicionCuatro +7;
+                cout<<serie<<" ";
+                m+=4;
+                posicionCuatro+=7;
+            }
 
-        i++;
+
+            i++;
+        }
     }
 
     return 0;
